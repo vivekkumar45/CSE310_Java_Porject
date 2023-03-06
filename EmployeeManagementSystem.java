3eed4abf1a7b3980e36e3ae6e4eb698ae1fcd944
Devesh Kundu <devesh.kundu28@gmail.com>
2023-03-06 21:58:07 +0530

    Create EmployeeManagementSystem.java

---
+++ EmployeeManagementSystem.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class EmployeeManagementSystem {
+	// define constants for tax and national insurance rates
+	private static final double TAX_RATE = 0.2;
+	private static final double NATIONAL_INSURANCE_RATE = 0.1;
+	
+	// define an Employee class to store employee details
+	
+	// define a PaySlip class to store pay slip details
+	
+	
+	public static void main(String args[]) {
+		
+	}
+}
