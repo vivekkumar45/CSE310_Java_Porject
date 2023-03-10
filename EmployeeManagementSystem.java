071ed758a97a8db0d29658424a918530bf2c91aa
Devesh Kundu <devesh.kundu28@gmail.com>
2023-03-10 19:03:05 +0530

    Update EmployeeManagementSystem.java

--- EmployeeManagementSystem.java
+++ EmployeeManagementSystem.java
@@ -6,6 +6,33 @@ public class EmployeeManagementSystem {
 	private static final double NATIONAL_INSURANCE_RATE = 0.1;
 	
 	// define an Employee class to store employee details
+	private static class Employee {
+		private String name;
+		private int id;
+		private double salary;
+		
+		public Employee(String name, int id, double salary) {
+			this.name = name;
+			this.id = id;
+			this.salary = salary;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		
+		public int getId() {
+			return id;
+		}
+		
+		public double getSalary() {
+			return salary;
+		}
+		
+		public void setSalary(double salary) {
+			this.salary = salary;
+		}
+	}
 	
 	// define a PaySlip class to store pay slip details
 	
