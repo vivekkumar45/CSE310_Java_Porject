b6d2c748a6a55f732175e58cb7a611c2851a7474
vivekkumar45 <92422766+vivekkumar45@users.noreply.github.com>
2023-04-13 21:10:14 +0530

    Update EmployeeManagementSystem.java

--- EmployeeManagementSystem.java
+++ EmployeeManagementSystem.java
@@ -6,6 +6,14 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+class Employee {
+    private static int nextId = 1;
+    private final int id;
+    private final String name;
+    private double salary;
+    private double taxRate;
+    private double niRate;
+
 public class EmployeeManagementSystem {
     public static void main(String[] args) {
         
